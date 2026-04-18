f3876302ee7c501cce6ea30c07451985a85f48e8
DoctorLaplace <silvervalifor@gmail.com>
2026-04-17 21:26:47 -0700

    [Fix] Defined Colors class in dataset.py to resolve NameError during scanning.

--- embedding_creation/dataset.py
+++ embedding_creation/dataset.py
@@ -15,6 +15,17 @@ from torch.utils.data import Dataset
 import threading
 
 
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+
+
 class AvirisSSLDataset(Dataset):
     """
     Self-supervised pre-training dataset.
